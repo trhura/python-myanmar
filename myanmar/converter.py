a63a144bac389759c3b1be884bf5be7bbd32c7fb
Thura Hlaing <trhura@gmail.com>
2013-04-02 12:11:01 +0630

    added basic syllable itr

--- myanmar/converter.py
+++ myanmar/converter.py
@@ -1,5 +1,6 @@
 import os.path
 import json
+import re
 
 class BaseEncoding (object):
 
@@ -7,15 +8,7 @@ class BaseEncoding (object):
         """
         """
         self.mappings = self.load_json (jsonFile)
-
-        self.mappings["pre-diacritics"] = {}
-        for i in ["eVowel", "yayit", "kinzi"]:
-            self.mappings["pre-diacritics"].update (self.mappings[i])
-        self.mappings["post-diacritics"] = {}
-        for i in ["stack", "yapin", "iVowel", "uVowel", "anusvara",
-                  "aaVowel", "dot_below", "asat", "visarga"]:
-            self.mappings["post-diacritics"].update (self.mappings[i])
-
+        self.update_mappings ()
         self.pattern = self.get_pattern ()
         print (self.pattern)
 
@@ -44,10 +37,27 @@ class BaseEncoding (object):
             mappings = json.load (iFile)
             return mappings
 
+    def update_mappings (self): pass
+
 class UnicodeEncoding (BaseEncoding):
     def __init__ (self, *args, **kwargs):
-        self.syllable_pattern = []
+        self.syllable_pattern = [
+            "independent",
+            "digits",
+            "puncts",
+            [("pre_diacritics",), "cons", ("post_diacritics",)],
+            ]
         super ().__init__(*args, **kwargs)
+
+    def update_mappings (self):
+        self.mappings["pre_diacritics"] = {}
+        for i in ["kinzi"]:
+            self.mappings["pre_diacritics"].update (self.mappings[i])
+
+        self.mappings["post_diacritics"] = {}
+        for i in ["stack", "yayit", "yapin", "wasway", "hatoh", "eVowel", "iVowel",
+                  "uVowel", "anusvara", "aaVowel", "dot_below", "asat", "visarga"]:
+            self.mappings["post_diacritics"].update (self.mappings[i])
 
 class ZawgyiEncoding (BaseEncoding):
     def __init__ (self, *args, **kwargs):
@@ -56,13 +66,43 @@ class ZawgyiEncoding (BaseEncoding):
             "digits",
             "puncts",
             "lig",
-            [("pre-diacritics",), "cons", ("post-diacritics",)],
+            [("pre_diacritics",), "cons", ("post_diacritics",)],
         ]
         super ().__init__(*args, **kwargs)
+
+    def update_mappings (self):
+        self.mappings["pre_diacritics"] = {}
+        for i in ["eVowel", "yayit", "kinzi"]:
+            self.mappings["pre_diacritics"].update (self.mappings[i])
+
+        self.mappings["post_diacritics"] = {}
+        for i in ["stack", "yapin", "wasway", "hatoh", "iVowel", "uVowel",
+                  "anusvara", "aaVowel", "dot_below", "asat", "visarga"]:
+            self.mappings["post_diacritics"].update (self.mappings[i])
+
+class SyllableIter ():
+
+    def __init__ (self, text="", encoding=UnicodeEncoding('data/unicode.json')):
+        self.text = text
+        self.pattern  = re.compile (encoding.get_pattern (), re.UNICODE)
+
+    def __iter__ (self):
+        return self
+
+    def __next__ (self):
+        for match in self.pattern.finditer (self.text):
+            return match.start(), match.end()
+
 
 def main  ():
     uni = UnicodeEncoding ('data/unicode.json')
     zgy = ZawgyiEncoding ('data/zawgyi.json')
+
+    with open ('data/test.txt', mode='r', encoding='utf-8') as iFile:
+        data = iFile.read ()
+        itr = SyllableIter (text=data, encoding=zgy)
+
+        print (data, itr.pattern.pattern)
 
     # import pprint
     # pprint.pprint (uni.mappings)
