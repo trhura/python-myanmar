4041e941d15c47e03a09f7e5512166bbb7f18ca9
Thura Hlaing <trhura@gmail.com>
2013-04-08 10:31:11 +0630

    initial code for choosing proper shaping for legacy encodings

--- myanmar/converter.py
+++ myanmar/converter.py
@@ -54,16 +54,27 @@ def convert (text, from_encoding, to_encoding):
         syllable_pattern = list(itertools.chain(*syllable_pattern))
 
         # collect codepoints in syllable, in correct syllable order
-        #print(syllable_pattern)
         syllable = ""
         for each_pattern in syllable_pattern:
-            if each_pattern in each_syllable:
-                key = from_encoding.reverse_table[each_syllable[each_pattern]]
-                syllable += to_encoding.table[key]
-                #print (str(each_syllable) + '\t' + syllable)
+            if not each_pattern in each_syllable:
+                continue
+
+            key = from_encoding.reverse_table[each_syllable[each_pattern]]
+            char = to_encoding.table[key]
+
+            if each_pattern == "yayit":
+                char = choose_yayit (each_syllable, to_encoding.table)
+            syllable += char
+
         otext += syllable
 
+    from pprint import pprint
+    #pprint (to_encoding.table)
+    #pprint (from_encoding.reverse_table)
     return otext
+
+def choose_yayit (syllable, table):
+    return table['yayit']
 
 def main  ():
     with open ('data/test2.txt', mode='r', encoding='utf-8') as iFile:
