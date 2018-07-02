65b92468a46ef75306f7c5f15b7085c844b1a550
Thura Hlaing <trhura@gmail.com>
2018-07-02 09:56:01 +0630

    Bump version: 1.1.0 → 1.2.0

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/trhura/python-myanmar',
-    version='1.1.0',
+    version='1.2.0',
     zip_safe=False,
     package_data={
         'myanmar': ['data/*.json'],
