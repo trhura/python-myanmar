7facbc4c0c261e28fe9c7fcc5d3f281067a4a50d
Thura Hlaing <thura.hlaing@frontiir.com>
2019-05-03 15:43:18 +0630

    Bump version: 1.6.0 → 1.7.0

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/trhura/python-myanmar',
-    version='1.6.0',
+    version='1.7.0',
     zip_safe=False,
     package_data={
         'myanmar': ['data/*.json'],
